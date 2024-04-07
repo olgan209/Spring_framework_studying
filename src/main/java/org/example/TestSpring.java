a920f5ee7ea8cbbc0998215f1a57f11cc53b863b
Olga <olganeboga209@gmail.com>
2024-04-07 12:49:44 +0600

    lesson 2 is finished

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,11 +1,15 @@
 package org.example;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+//благодаря тому что мы импортировали spring context мы можес обращаться к ClassPathXmlApplicationContext
 public class TestSpring {
     public static void main(String[] args){
-        ClassPathXmlApplicationContext contex = new ClassPathXmlApplicationContext(
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class); // указываем обьект какого класса мы хотим получить, в нашем случае объект класса TestBean
+        System.out.println(testBean.getName());
+        context.close();
     }
 }
