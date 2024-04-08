cbc77f80c2a21f539529d207cb4bb92074d1b93b
Olga <olganeboga209@gmail.com>
2024-04-08 10:55:54 +0600

    lesson 8 finished

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,9 +1,20 @@
 package org.example;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){}
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+    public void doMyInit(){
+        System.out.println("Doing my init");
+    }
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong(){
         return "Hungarian Rhapsody";
     }
+
 
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,13 +8,16 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
